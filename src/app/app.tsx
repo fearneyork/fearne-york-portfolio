21586652ff54c199a6282aa54b4dd87e135cf998
Fearne York <fearneyork@gmail.com>
2022-12-30 12:44:10 +0000

    initial home commit and tweak to test runner script

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Cv } from '@fearne-york-portfolio/cv';
+import {Link, Route, Routes} from 'react-router-dom'
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="fearne-york-portfolio" />
-      <div />
-    </>
+      <Routes>
+        <NxWelcome title="fearne-york-portfolio" />
+        <div />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cv" element={<Cv />}></Route>
+      </Routes>
   );
 }
 
 export default App;
+
+
+const Home = () => {
+ return (
+  <div className={styles['container']}>
+    <Link to="/cv">View CV</Link>
+  </div>
+ )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
+    <BrowserRouter></BrowserRouter>
     <App />
   </StrictMode>
 );
